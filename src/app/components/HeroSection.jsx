9eefeefdf9f33095cab0cf0b80d4d4072736cf8b
AD GILL <adgill.127.0.0.1@gmail.com>
2023-11-23 15:09:07 +0530

    second commit

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -42,8 +42,9 @@ const HeroSection = () => {
             >
               Hire Me
             </Link> */}
+
             <Link
-              href="/"
+              href="https://wa.me/message/UA3MVFZYMM7IH1"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
