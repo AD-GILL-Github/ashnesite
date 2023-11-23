00d7d3c9e035ee8fcefd6af98e511ba1192f1bb0
AD GILL <adgill.127.0.0.1@gmail.com>
2023-11-23 14:44:54 +0530

    first commit

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,96 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Facebook AD&apos;s</li>
+        <li>Google AD&apos;s</li>
+        <li>instagram AD&apos;s</li>
+        <li>Toktok AD&apos;s</li>
+        <li>Funnel Creation</li>
+        <li>Landing Pages</li>
+        
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Fullstack Academy of Code</li>
+        <li>University of California, Santa Cruz</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Official meta partener certified</li>
+        <li>Google marketing ans SEO professionals certified</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/aboutimg.png" alt="" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">OUR WORK CULTURE</h2>
+          <p className="text-base lg:text-lg">
+            
+Welcome to ASHNE OS, where creativity meets strategy to elevate your brand. Our team of innovative minds is dedicated to crafting compelling campaigns that make a lasting impact. From concept to execution, we specialize in turning ideas into results. Explore our services and let&apos;s redefine your brand story together.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              services{" "}
+            </TabButton>
+            {/* <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton> */}
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
